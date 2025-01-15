fd4e8eefed9f2ce2431d09beabc415b05b8c7d1b
Bimo P Reksoprodjo <bimo1505@gmail.com>
2025-01-15 11:35:50 +0700

    Add page title validator

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -13,20 +13,26 @@ test('Test Case #1', async ({ page }) => {
 
     // Expect login to be successful
     await expect(page).toHaveURL('/inventory.html');
+    expect(page.locator('[data-test="title"]')).toHaveText("Products");
+
+    // Add one item to cart
+    await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
 
     // Get item details
     const itemName = page.locator('[data-test="item-1-title-link"]').textContent;
     const itemDescription = page.getByText('Get your testing superhero on').textContent;
     const itemPrice = page.locator('div').filter({ hasText: /^\$15\.99Remove$/ }).locator('[data-test="inventory-item-price"]').textContent;
 
-    // Add one item to cart
-    await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
-
     // Click on shopping cart icon
     await page.locator('[data-test="shopping-cart-link"]').click();
 
+    // Expect page redirection to cart
+    await expect(page).toHaveURL('/cart.html');
+    expect(page.locator('[data-test="title"]')).toHaveText("Your Cart");
+
     // Verify item in cart
-    await expect(page.locator('[data-test="item-1-title-link"]').textContent).toBe(itemName);
-    await expect(page.locator('[data-test="inventory-item-desc"]').textContent).toBe(itemDescription);
-    await expect(page.locator('[data-test="inventory-item-price"]').textContent).toBe(itemPrice);
+    expect(page.locator('[data-test="item-quantity"]')).toHaveText("1");
+    expect(page.locator('[data-test="item-1-title-link"]').textContent).toBe(itemName);
+    expect(page.locator('[data-test="inventory-item-desc"]').textContent).toBe(itemDescription);
+    expect(page.locator('[data-test="inventory-item-price"]').textContent).toBe(itemPrice);
 });
