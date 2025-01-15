4042c81b026de60c5f3ab5d2b0537ca32d6c636e
Bimo P Reksoprodjo <bimo1505@gmail.com>
2025-01-15 10:09:09 +0700

    Linter setup

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -6,5 +6,11 @@ test('Automate login flow', async ({ page }) => {
 
     // Set credentials
     await page.locator('[data-test="username"]').fill("standard_user");
-    await page.locator('[data-test="password"]').fill("standard_user");
+    await page.locator('[data-test="password"]').fill("secret_sauce");
+
+    // Click Login
+    await page.locator('[data-test="login-button"]').click();
+
+    // Expect to be successful
+    expect(page.url()).toBe("https://www.saucedemo.com/inventory.html")
 });
